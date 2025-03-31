4610c14162d4c933acc68bcff17ef73a5a673e35
Efe Tascioglu <46626642+EfeTascioglu@users.noreply.github.com>
2025-03-30 22:58:43 -0400

    Update vite.config.ts for external reference

--- vue-project3/vite.config.ts
+++ vue-project3/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/AI-Ethics-Assessment-Portal/vue-project3/",
   plugins: [
     vue(),
     vueDevTools(),
